33c0f19883dfce1f65b07e5af819862647cb99b4
succ <2014649323@qq.cm>
2023-08-01 16:55:08 +0800

    编写菜单组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import Button, {ButtonSize, ButtonType}  from './components/Button/button';
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
     <div>
       <header>
+        <Menu defaultIndex={0}>
+          <MenuItem>
+            香蕉
+          </MenuItem>
+          <MenuItem>
+            香蕉
+          </MenuItem>
+          <MenuItem>
+            香蕉
+          </MenuItem>
+          <MenuItem>
+            香蕉
+          </MenuItem>
+        </Menu>
         <Button >button</Button>
         <Button btnType={ButtonType.Default} size={ButtonSize.Large}>button</Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>button</Button>
