e0ac113e3a0aa3371699fbe2ed244885a8a2c854
succ <2014649323@qq.cm>
2023-08-02 16:47:50 +0800

    fix

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -48,10 +48,10 @@ describe('test Menu and MenuItem component', () => {
         fireEvent.click(thirdItem)
         expect(thirdItem).toHaveClass('is-active')
         expect(activeElement).not.toHaveClass('is-active')
-        expect(testProps.onSelect).toHaveBeenCalledWith(2)
+        expect(testProps.onSelect).toHaveBeenCalledWith('2')
         fireEvent.click(disabledElement)
         expect(disabledElement).not.toHaveClass('is-active')
-        expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+        expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
     })
     it('should render vertical mode when mode is set to vertical', () => {
         cleanup()
